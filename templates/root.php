c69967270ad239ca8e900c2bffcf5542f7962905
Matthew Anderson <92887765+mjanderson1227@users.noreply.github.com>
2025-04-25 12:29:42 -0500

    removed script source from root.php

--- templates/root.php
+++ templates/root.php
@@ -12,7 +12,6 @@ if (! $page) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="/static?file=query.js"></script>
   <title>Query Builder</title>
   <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" defer></script>
